627da8f2a43421cde949b1f6d3da76fd5781f1a8
hbblo <hbblo@LAPTOP-UJKTBKSO>
2023-03-22 10:21:31 +0100

    Add project files.

---
+++ Ujemne,dodatnie/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ujemne_dodatnie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę");
+
+            string liczba = Console.ReadLine();
+
+            Console.WriteLine("Twoja liczba " + liczba);
+
+            int a = int.Parse(liczba);
+
+
+            if (a > 0)
+            {
+                Console.WriteLine(liczba + " jest liczbą dodatnią");
+
+
+
+            }
+            else Console.WriteLine(liczba + " jest liczbą ujemną");
+
+
+        }
+    }
+}
